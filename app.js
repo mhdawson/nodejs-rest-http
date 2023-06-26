8285f54d28f14815531c49b513a085549c463a3e
Michael Dawson <mdawson@devrus.com>
2023-06-26 15:20:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/greeting', (request, response) => {
   const name = request.query ? request.query.name : undefined;
-  response.send({ content: `Hello RHTAP, ${name || 'World!'}` });
+  response.send({ content: `Hello RHTAP2, ${name || 'World!'}` });
 });
 
 module.exports = app;
